6e4285aeb3289da41c14fb2755c09f77edbb2fa5
jlbuild <juliabuildbot@gmail.com>
2022-01-30 23:19:37 +0000

    blaspp_jll build 2021.4.2+0

--- src/wrappers/aarch64-linux-musl-cxx03.jl
+++ src/wrappers/aarch64-linux-musl-cxx03.jl
@@ -2,11 +2,11 @@
 export libblaspp
 
 using CompilerSupportLibraries_jll
-using libblastrampoline_jll
+using OpenBLAS32_jll
 JLLWrappers.@generate_wrapper_header("blaspp")
 JLLWrappers.@declare_library_product(libblaspp, "libblaspp.so")
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, libblastrampoline_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, OpenBLAS32_jll)
     JLLWrappers.@init_library_product(
         libblaspp,
         "lib/libblaspp.so",
